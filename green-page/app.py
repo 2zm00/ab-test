185d1b84fb98b760d72a1b00ef590f67725d2def
2zm00 <ljm010301@naver.com>
2025-02-26 10:03:54 +0900

    feat: 초록색 페이지 app, index 생성

---
+++ green-page/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, send_file, jsonify
+import redis
+import os
+
+app = Flask(__name__)
+# WSL의 IP 주소를 환경변수로 받음
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_client = redis.Redis(host=redis_host, port=6379)
+
+@app.route("/")
+def home():
+    return send_file("index.html")
+
+@app.route("/count", methods=["POST"])
+def increment_count():
+    redis_client.incr("green_visits")
+    return "OK"
+
+@app.route("/count")
+def get_count():
+    visits = int(redis_client.get("green_visits") or 0)
+    return jsonify({"visits": visits})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
